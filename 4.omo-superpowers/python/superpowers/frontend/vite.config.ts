0bd241d3413009dffc63b722700f4ddd0f3201aa
ran_lin <boydfd@163.com>
2026-02-27 12:15:32 +0000

    fea: move superpowers to it's folder.

--- 4.omo-superpowers/python/superpowers/frontend/vite.config.ts
+++ 4.omo-superpowers/python/superpowers/frontend/vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: [
+      'code.aboydfd.com',
+      '.code.aboydfd.com',
+      'localhost'
+    ],
+
     proxy: {
       '/api': 'http://localhost:8000'
     }
